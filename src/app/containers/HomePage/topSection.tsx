9ce4491dfcb282647744e8aedca92ade10c770ac
Isaac177 <isaacjogues177@gmail.com>
2022-10-24 21:04:18 +0600

    Hero buttons

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../assets/images/blob.svg";
 import {SCREENS} from "../../components/responsive";
+import {Button} from "../../components/button";
 
 
 const TopSectionContainer = styled.div`
@@ -138,6 +139,14 @@ const StandaloneCar = styled.div`
     }
 `;
 
+const ButtonsContainer = styled.div`
+    ${tw`
+        flex
+        flex-wrap
+        mt-4
+    `}
+`;
+
 export const TopSection = () => {
     return (
         <TopSectionContainer>
@@ -146,6 +155,10 @@ export const TopSection = () => {
                 <Description>
                     Always choose the best car from our local stores or order it remotely at the best price for you and get the best quality cars for as long as you like
                 </Description>
+                <ButtonsContainer>
+                    <Button text="Book Your Ride" />
+                    <Button theme="filled" text="Sell Your Car" />
+                </ButtonsContainer>
             </LeftContainer>
             <RightContainer>
                 <BlobContainer>
